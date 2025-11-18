40814f5cb96515539cfd33a9bca39cb451d6938c
Roman Gleizer <romy4.gleyzer@mail.ru>
2025-11-18 20:58:09 +0500

    Начальное проектирование

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,41 +1,74 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
-namespace ObjectPrinting
+namespace ObjectPrinting;
+
+public class PrintingConfig<TOwner>
 {
-    public class PrintingConfig<TOwner>
+    private readonly HashSet<Type> _excludedTypes = [];
+    private readonly HashSet<MemberInfo> _excludedMembers = [];
+    private readonly Dictionary<Type, Func<object, string>> _typeSerializers = [];
+    private readonly Dictionary<MemberInfo, Func<object, string>> _memberSerializers = [];
+    private readonly Dictionary<Type, CultureInfo> _typeCultures = [];
+    private readonly Dictionary<MemberInfo, CultureInfo> _memberCultures = [];
+    private readonly Dictionary<MemberInfo, int> _memberStringMaxLength = [];
+    private readonly int? _defaultStringMaxLength;
+
+    public PrintingConfig<TOwner> Excluding<TPropType>()
     {
-        public string PrintToString(TOwner obj)
+        return this;
+    }
+
+    public PrintingConfig<TOwner> Excluding<TProp>(Expression<Func<TOwner, TProp>> memberSelector)
+    {
+        return this;
+    }
+
+    public PropertyPrintingConfig<TOwner, TProp> Printing<TProp>()
+    {
+        return new PropertyPrintingConfig<TOwner, TProp>(this, null);
+    }
+
+    public PropertyPrintingConfig<TOwner, TProp> Printing<TProp>(Expression<Func<TOwner, TProp>> memberSelector)
+    {
+        return null!;
+        // return new PropertyPrintingConfig<TOwner, TProp>(this, /*MemberInfo объект*/);
+    }
+
+    public string PrintToString(TOwner obj)
+    {
+        return PrintToString(obj, 0);
+    }
+
+    private string PrintToString(object obj, int nestingLevel)
+    {
+        //TODO apply configurations
+        if (obj == null)
+            return "null" + Environment.NewLine;
+
+        var finalTypes = new[]
         {
-            return PrintToString(obj, 0);
-        }
+            typeof(int), typeof(double), typeof(float), typeof(string),
+            typeof(DateTime), typeof(TimeSpan)
+        };
+        if (finalTypes.Contains(obj.GetType()))
+            return obj + Environment.NewLine;
 
-        private string PrintToString(object obj, int nestingLevel)
+        var identation = new string('\t', nestingLevel + 1);
+        var sb = new StringBuilder();
+        var type = obj.GetType();
+        sb.AppendLine(type.Name);
+        foreach (var propertyInfo in type.GetProperties())
         {
-            //TODO apply configurations
-            if (obj == null)
-                return "null" + Environment.NewLine;
-
-            var finalTypes = new[]
-            {
-                typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
-            };
-            if (finalTypes.Contains(obj.GetType()))
-                return obj + Environment.NewLine;
-
-            var identation = new string('\t', nestingLevel + 1);
-            var sb = new StringBuilder();
-            var type = obj.GetType();
-            sb.AppendLine(type.Name);
-            foreach (var propertyInfo in type.GetProperties())
-            {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
-            }
-            return sb.ToString();
+            sb.Append(identation + propertyInfo.Name + " = " +
+                      PrintToString(propertyInfo.GetValue(obj),
+                          nestingLevel + 1));
         }
+        return sb.ToString();
     }
 }
---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace ObjectPrinting;
+
+public class PropertyPrintingConfig<TOwner, TProp>(PrintingConfig<TOwner> printingConfig, MemberInfo? memberInfo)
+{
+    private readonly PrintingConfig<TOwner> _parentConfig = printingConfig;
+    private readonly MemberInfo? _memberInfo = memberInfo;
+
+    public PrintingConfig<TOwner> Using(Func<TProp, string> serializer)
+    {
+        /*
+         * Если _memberInfo null, тогда работаем со всеми значениями типа TProp
+         * Иначе только с конкретным свойством
+         */
+
+        return _parentConfig;
+    }
+
+    public PrintingConfig<TOwner> Using(CultureInfo c)
+    {
+        /*
+         * Если _memberInfo null, тогда работаем со всеми значениями типа TProp
+         * Иначе только с конкретным свойством
+         */
+
+        return _parentConfig;
+    }
+
+    public PrintingConfig<TOwner> TrimmedToLength(int len)
+    {
+        /*
+         * Если _memberInfo null, тогда устанавливаем длину для обычной строки
+         * Иначе только для конткретного свойства
+         */
+
+        return _parentConfig;
+    }
+}
