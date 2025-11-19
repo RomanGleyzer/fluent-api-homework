cbc3f74fa09ca1176fec556298464c15f8c5bdf1
Roman Gleizer <romy4.gleyzer@mail.ru>
2025-11-19 18:50:42 +0500

    Реализация PrintingConfig

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,7 +1,5 @@
-using System;
+Ôªøusing System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
@@ -14,18 +12,21 @@ public class PrintingConfig<TOwner>
     private readonly HashSet<MemberInfo> _excludedMembers = [];
     private readonly Dictionary<Type, Func<object, string>> _typeSerializers = [];
     private readonly Dictionary<MemberInfo, Func<object, string>> _memberSerializers = [];
-    private readonly Dictionary<Type, CultureInfo> _typeCultures = [];
-    private readonly Dictionary<MemberInfo, CultureInfo> _memberCultures = [];
-    private readonly Dictionary<MemberInfo, int> _memberStringMaxLength = [];
-    private readonly int? _defaultStringMaxLength;
 
     public PrintingConfig<TOwner> Excluding<TPropType>()
     {
+        _excludedTypes.Add(typeof(TPropType));
         return this;
     }
 
     public PrintingConfig<TOwner> Excluding<TProp>(Expression<Func<TOwner, TProp>> memberSelector)
     {
+        ArgumentNullException.ThrowIfNull(memberSelector);
+
+        if (memberSelector.Body is not MemberExpression expression)
+            throw new ArgumentException(null, nameof(memberSelector));
+
+        _excludedMembers.Add(expression.Member);
         return this;
     }
 
@@ -36,39 +37,94 @@ public class PrintingConfig<TOwner>
 
     public PropertyPrintingConfig<TOwner, TProp> Printing<TProp>(Expression<Func<TOwner, TProp>> memberSelector)
     {
-        return null!;
-        // return new PropertyPrintingConfig<TOwner, TProp>(this, /*MemberInfo Ó·˙ÂÍÚ*/);
+        ArgumentNullException.ThrowIfNull(memberSelector);
+
+        if (memberSelector.Body is not MemberExpression expression)
+            throw new ArgumentException(null, nameof(memberSelector));
+
+        return new PropertyPrintingConfig<TOwner, TProp>(this, expression.Member);
     }
 
     public string PrintToString(TOwner obj)
     {
-        return PrintToString(obj, 0);
+        var visited = new HashSet<object>();
+        return PrintToString(obj, 0, visited, null);
     }
 
-    private string PrintToString(object obj, int nestingLevel)
+    private string PrintToString(object obj, int nestingLevel, HashSet<object> visited, MemberInfo? member)
     {
-        //TODO apply configurations
         if (obj == null)
             return "null" + Environment.NewLine;
 
-        var finalTypes = new[]
+        var type = obj.GetType();
+
+        if (!IsFinalType(type))
         {
-            typeof(int), typeof(double), typeof(float), typeof(string),
-            typeof(DateTime), typeof(TimeSpan)
-        };
-        if (finalTypes.Contains(obj.GetType()))
+            if (visited.Contains(type))
+                return "Cycle ref" + Environment.NewLine;
+
+            visited.Add(obj);
+        }
+
+        if (member != null && _memberSerializers.TryGetValue(type, out var memberSerializer))
+            return memberSerializer(obj) + Environment.NewLine;
+
+        if (_typeSerializers.TryGetValue(type, out var typeSerializer))
+            return typeSerializer(obj) + Environment.NewLine;
+
+        if (IsFinalType(type))
             return obj + Environment.NewLine;
 
         var identation = new string('\t', nestingLevel + 1);
         var sb = new StringBuilder();
-        var type = obj.GetType();
+
         sb.AppendLine(type.Name);
+
         foreach (var propertyInfo in type.GetProperties())
         {
-            sb.Append(identation + propertyInfo.Name + " = " +
-                      PrintToString(propertyInfo.GetValue(obj),
-                          nestingLevel + 1));
+            if (ShouldSkipMember(propertyInfo))
+                continue;
+
+            var val = propertyInfo.GetValue(obj);
+            sb.Append(
+                identation 
+                + propertyInfo.Name 
+                + " = " 
+                + PrintToString(val, nestingLevel + 1, visited, propertyInfo)
+                );
         }
+
         return sb.ToString();
+    }
+
+    private static bool IsFinalType(Type type)
+    {
+        return type.IsPrimitive
+            || type == typeof(string)
+            || type == typeof(decimal)
+            || type == typeof(DateTime)
+            || type == typeof(TimeSpan)
+            || type == typeof(Guid);
+    }
+
+    private bool ShouldSkipMember(MemberInfo member)
+    {
+        if (_excludedMembers.Contains(member))
+            return true;
+
+        var type = member switch
+        {
+            PropertyInfo p => p.PropertyType,
+            FieldInfo f => f.FieldType,
+            _ => null
+        };
+
+        if (type == null)
+            return false;
+
+        if (_excludedTypes.Contains(type))
+            return true;
+
+        return false;
     }
 }
